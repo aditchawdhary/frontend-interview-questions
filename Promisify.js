5a019f45f7be464444fcc917560dfd786fc23013
Adit Chawdhary <25533953+aditchawdhary@users.noreply.github.com>
2026-02-24 12:34:31 -0800

    Add promisify function for async API handling
    Implement promisify function to convert callback-based APIs to promise-based.

---
+++ Promisify.js
@@ -0,0 +1,34 @@
+/*
+Promisify
+Before promises/async/await became the standard, it was a convention for async APIs in JavaScript to accept callbacks as the last argument. 
+Many async versions of Node.js APIs (e.g. fs.readFile and fs.rm) have such signatures. Node.js' util.promisify function was created to wrap around callback-based functions by 
+returning Promises so that they can be used with async/await.
+
+Implement a function promisify that takes a function following the common callback-last error-first style, i.e. taking a (err, value) => ... callback as the last argument, and returns a version that returns promises.
+
+Examples
+
+// Example function with callback as last argument
+// The callback has the signature `(err, value) => any`
+function foo(url, options, callback) {
+  apiCall(url, options)
+    .then((data) => callback(null, data))
+    .catch((err) => callback(err));
+}
+
+const promisifiedFoo = promisify(foo);
+const data = await promisifiedFoo('example.com', { foo: 1 });
+
+*/
+
+/**
+ * @callback func
+ * @returns Function
+ */
+export default function promisify(func) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      func.apply(this, [...args, (err, result) => err ? reject(err) : resolve(result)]);
+    });
+  }
+}
