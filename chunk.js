450df96d244edb7b5ec287fb1c974a225ece16d9
Adit Chawdhary <25533953+aditchawdhary@users.noreply.github.com>
2026-02-25 13:02:54 -0800

    Create chunk.js

---
+++ chunk.js
@@ -0,0 +1,38 @@
+/*
+Chunk
+Implement a function chunk(array, [size=1]) that splits the input array into groups of length size and returns them within a new array. If array can't be split evenly, the final chunk will be the remaining elements. The function should not modify the original input array.
+
+Arguments
+array (Array): The array to process.
+[size=1] (number): The length of each chunk.
+Returns
+(Array): Returns the new array of chunks.
+
+Examples
+
+chunk(['a', 'b', 'c', 'd']); // => [['a'], ['b'], ['c'], ['d']]
+chunk([1, 2, 3, 4], 2); // => [[1, 2], [3, 4]]
+chunk([1, 2, 3, 4], 3); // => [[1, 2, 3], [4]]
+The function should return an empty array if the array argument is empty.
+
+Constraints
+size >= 1
+0 <= length of array <= 100
+Resources
+Lodash _.chunk - https://lodash.com/docs/#chunk
+*/
+
+/**
+ * @template T
+ * @param {Array<T>} array The array to process.
+ * @param {number} [size=1] The length of each chunk.
+ * @returns {Array<Array<T>>} The new array of chunks.
+ */
+export default function chunk(array, size = 1) {
+  let newArray = new Array();
+  for (let i = 0; i < array.length; i += size) {
+    newArray.push(array.slice(i, i + size));
+  }
+  return newArray;
+}
+
